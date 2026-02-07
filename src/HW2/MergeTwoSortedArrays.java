d15d1b43ea351233d3a7fabf2cf6df4c580467be
Lauren <chih.liu@sjsu.edu>
2026-02-06 18:06:51 -0800

    Added comments

--- src/HW2/MergeTwoSortedArrays.java
+++ src/HW2/MergeTwoSortedArrays.java
@@ -2,23 +2,27 @@ package HW2;
 
 public class MergeTwoSortedArrays {
     public static int[] merge(int[] A, int[] B) {
-        // Implementation of merge logic goes here
-        // (e.g., using two pointers and a result array)
+
+        // i: index for A, j: index for B, k: index for merged array
         int i = 0, j = 0, k = 0;
+        // merged array length = total length of both arrays
         int[] merged = new int[A.length + B.length];
 
+        // compare current elements of A and B, put smaller one into merged
+        // if equal, take from A first
         while (i < A.length && j < B.length) {
-            if (A[i] < B[j]) {
+            if (A[i] <= B[j]) {
                 merged[k++] = A[i++];
             } else {
                 merged[k++] = B[j++];
             }
         }
 
+        // copy any remaining elements in A
         while (i < A.length) {
             merged[k++] = A[i++];
         }
-
+        // copy any remaining elements in B
         while (j < B.length) {
             merged[k++] = B[j++];
         }
